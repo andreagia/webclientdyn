2cb573002910c2c39668c0334ac735ac5a395ab4
andrea <andreagia71@gmail.com>
2017-07-28 09:51:09 +0200

    Initial commit

---
+++ src/main/java/org/cirmmp/webclientdyn/authentication/CustomUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package org.cirmmp.webclientdyn.authentication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    Logger logger = LoggerFactory.getLogger(CustomUsernamePasswordAuthenticationFilter.class);
+
+
+    // ~ Constructors
+    // ===================================================================================================
+
+    public CustomUsernamePasswordAuthenticationFilter(){
+        super();
+      //  setPostOnly(false);
+        setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/login", "GET"));
+    }
+
+
+}
